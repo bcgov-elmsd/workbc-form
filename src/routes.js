e526853ef23bb72af0cd7332c0cc4c7691be12d9
Solorzano <Rafael.Solorzano@gov.bc.ca>
2020-03-27 14:28:45 -0700

    initial complete validation and email html
    initial complete validation and email html generator

--- src/routes.js
+++ src/routes.js
@@ -69,7 +69,13 @@ router.post(
       .withMessage("Please enter a valid Postal Code."),
       check("phone")
       .isMobilePhone(['en-CA','en-US'])
-      .withMessage("Please enter a valid phone number.")
+      .withMessage("Please enter a valid phone number."),
+      check("consent")
+      .notEmpty()
+      .withMessage("You must agree before submitting."),
+      check("catchment")
+      .notEmpty()
+      .withMessage("Please select at least one region.")
   ],
   (req, res) => {
     console.log(req.body);
@@ -85,6 +91,10 @@ router.post(
 
     const data = matchedData(req);
     console.log("Sanitized: ", data);
+    console.log(data.firstname);
+
+    
+
     // Homework: send sanitized data in an email or persist in a db
     let transporter = nodemailer.createTransport({
       host: "apps.smtp.gov.bc.ca",
@@ -95,22 +105,49 @@ router.post(
       } // true for 465, false for other ports
     });
   
+    var htmlMessage = "";
+    htmlMessage += "<p>" + data.firstname + "</p>";
+
     // send mail with defined transport object
-    /*
+    
     let info = transporter.sendMail({
-      from: '"A ghost" <workbctest@gov.bc.ca>', // sender address
-      to: "Webmaster <rafael.solorzano@gov.bc.ca>", // list of receivers
-      subject: "Hello", // Subject line
-      text: "Hello world?", // plain text body
-      html: "<b>Hello world?</b>" // html body
+      from: '"Job Seeker" <donotreply@gov.bc.ca>', // sender address
+      to: "WorkBC Jobs <WorkBCJobs@gov.bc.ca>", // list of receivers
+      subject: "Job Seeker Form", // Subject line
+      text: "Plain text", // plain text body
+      html: createJobSeekerHtml(data) // html body
     });
-    */
+    
+    
   
-    //console.log("Message sent: %s", info.messageId);
+    console.log("Message sent: %s", info.messageId);
 
     req.flash("success", "Form has been submitted");
     res.redirect("/done");
   }
 );
+
+function createJobSeekerHtml(data){
+  var html = "";
+  html += "<h2>A Job Seeker Form has been submitted</h2>"
+  html += "<p>First Name: " + data.firstname + "</p>"
+  html += "<p>Middle Name: " + data.middlename ? data.middlename : "" + "</p>"
+  html += "<p>Last Name: "+ data.lastname + "</p>"
+  html += "<p>Phone: "+ data.phone + "</p>"
+  html += "<p>Email: "+ data.email + "</p>"
+  html += "<p>Address: "+ data.address ? data.address : "" + "</p>"
+  html += "<p>Address2: "+ data.address2? data.address2 : "" + "</p>"
+  html += "<p>City: "+ data.city ? data.city : "" + "</p>"
+  html += "<p>Postal Code: "+ data.postal + "</p>"
+  html += "<p>Region(s) available to work: "+ data.catchment + "</p>"
+  return html;
+  /*
+  html += "<p>"+ "</p>"
+  html += "<p>"+ "</p>"
+  html += "<p>"+ "</p>"
+  html += "<p>"+ "</p>"
+  html += "<p>"+ "</p>"
+  */
+}
 
 module.exports = router
