a9746e8887d2abc6fca0994b23205f7325c779f3
Solorzano <Rafael.Solorzano@gov.bc.ca>
2020-04-20 16:40:31 -0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -28,7 +28,10 @@ redisClient.on("error", function(error) {
   console.error(error);
 });
 
-
+app.use(function(req, res, next) {
+  res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+  next();
+});
 
 const middlewares = [
   helmet(),
@@ -48,10 +51,6 @@ const middlewares = [
 ];
 app.use(middlewares);
 
-app.use(function(req, res, next) {
-  res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
-  next();
-});
 
 app.use("/", routes);
 
